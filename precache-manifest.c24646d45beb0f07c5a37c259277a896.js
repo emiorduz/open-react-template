c82a19c7a64cc02dbb01b48e919980bd37a7dbea
Emilyuwu0 <tbarrera679@gmail.com>
2022-11-14 19:46:13 -0500

    Updates

---
+++ precache-manifest.c24646d45beb0f07c5a37c259277a896.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9223a82f3cbe5f0007eccd737c3ea80c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20c98fcbccbde9de9be4",
+    "url": "/static/css/main.0d3b219b.chunk.css"
+  },
+  {
+    "revision": "52d511326caf55aef35d",
+    "url": "/static/js/2.2cfc9d23.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.2cfc9d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c98fcbccbde9de9be4",
+    "url": "/static/js/main.a40127aa.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "79cfcc9cd613eba58c175cd5e641577b",
+    "url": "/static/media/BANDA_GRATIS.79cfcc9c.png"
+  },
+  {
+    "revision": "b5403bff0f31ebfd6371da5a337334db",
+    "url": "/static/media/MAFIA_DIAMOND.b5403bff.png"
+  },
+  {
+    "revision": "70269416f525a747d2d2d0b0c982c79b",
+    "url": "/static/media/MAFIA_VIP.70269416.png"
+  },
+  {
+    "revision": "23e3644d0b40b4d4607ac2f9d64ff69a",
+    "url": "/static/media/Normativas-LSPD.23e3644d.pdf"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "ed51bf48206b26e9a1cce5f0538439fc",
+    "url": "/static/media/sinneon.ed51bf48.png"
+  },
+  {
+    "revision": "c468257bace6b704d7fe3e5acf344fc1",
+    "url": "/static/media/uwu.c468257b.png"
+  }
+]);
